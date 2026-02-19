7f8f3124b43b621cdfed7db3c239f414795d30d5
joao.teodoro <joao.teodoro@atak.com.br>
2026-02-19 17:59:58 -0300

    Adicionando repository padrão

---
+++ EasyClothing.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyClothing.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ EasyClothing.Infra/Repositories/Repository.cs
@@ -0,0 +1,67 @@
+﻿using EasyClothing.Infra.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyClothing.Domain.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly EasyClothingDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public Repository(EasyClothingDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> AddAsync(
+    TEntity entity,
+    CancellationToken cancellationToken = default)
+        {
+            await _dbSet.AddAsync(entity, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return entity;
+        }
+
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(
+            TEntity entity,
+            CancellationToken cancellationToken = default)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(
+            TEntity entity,
+            CancellationToken cancellationToken = default)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<TEntity?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FindAsync(
+                new object[] { id },
+                cancellationToken
+            );
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.ToListAsync(cancellationToken);
+        }
+
+    }
+}
